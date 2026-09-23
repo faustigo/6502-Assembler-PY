01de408bb978acc2dca06897c04df47034376b2e
faustigo <gbgpf8@gmail.com>
2026-09-23 12:27:46 -0400

    feature: Added ability for objdump.output_obj to write to external file

--- src/my6502/objdump.py
+++ src/my6502/objdump.py
@@ -1,6 +1,7 @@
 from my6502.obj import ObjectCode
+import sys
 
-def output_obj(objcode: ObjectCode) -> None:
+def output_obj(objcode: ObjectCode, out = None, indent: str = "    ") -> None:
     # Create map of label_num => label str
     label_name_to_num = {}
     for label_name, label_num in objcode.labels.items():
@@ -8,9 +9,9 @@ def output_obj(objcode: ObjectCode) -> None:
 
     for idx, bs in enumerate(objcode.code_bytes):
         if idx in label_name_to_num:
-            print(f"{label_name_to_num[idx]}:")
+            print(f"{label_name_to_num[idx]}:", file=out)
         b_str = " ".join([f"{x:02X}" for x in bs])
         if idx in objcode.offsets_to_resolve:
             b_str += f" {objcode.offsets_to_resolve[idx]}"
-        print(f"\t{b_str}")
+        print(f"{indent}{b_str}", file=out)
 
--- tests/build_and_dump.py
+++ tests/build_and_dump.py
@@ -14,3 +14,9 @@ with objpath.open(mode="rb") as f:
     objcode = ObjectCode.from_file(f, objpath)
 
 output_obj(objcode)
+
+dumppath = Path(r".\out\x_loop.o.lst")
+if dumppath.is_dir():
+    raise Exception(f"ASSERT: dumppath {dumppath} is a directory")
+with dumppath.open(mode="w") as f:
+    output_obj(objcode, out=f)
